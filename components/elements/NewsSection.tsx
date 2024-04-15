2845f747bf7052dc7e80455ad2a4adc4d419ebb3
Vito Andareas Manik <vitoandareas15@gmail.com>
2024-04-15 21:51:12 +0800

    fix(NewsSection): removing Link component

--- components/elements/NewsSection.tsx
+++ components/elements/NewsSection.tsx
@@ -32,27 +32,25 @@ const NewsSection: React.FC<NewsSectionProps> = () => {
         <div className=" space-y-10 py-4 px-1 lg:px-0">
             <BentoGrid className=" mx-auto">
                 {data.data.map((item, i) => (
-                    <Link href={`/news/manik/${item.slug}`} key={uuidV4()}>
-                        <BentoGridItem
-                            title={item.title}
-                            description={""}
-                            header={
-                                <NewsImage
-                                    src={item.thumbnail}
-                                    alt="Burger"
-                                    key={uuidV4()}
-                                />
-                            }
-                            icon={
-                                <span className="text-xs text-muted-foreground">
-                                    {pubDate(item.pubDate!)}
-                                </span>
-                            }
-                            className={
-                                i === 3 || i === 6 ? "md:col-span-2" : ""
-                            }
-                        />
-                    </Link>
+                    <BentoGridItem
+                        title={item.title}
+                        description={""}
+                        href={`/news/manik/${item.slug}`}
+                        key={uuidV4()}
+                        header={
+                            <NewsImage
+                                src={item.thumbnail}
+                                alt="Burger"
+                                key={uuidV4()}
+                            />
+                        }
+                        icon={
+                            <span className="text-xs text-muted-foreground">
+                                {pubDate(item.pubDate!)}
+                            </span>
+                        }
+                        className={i === 3 || i === 6 ? "md:col-span-2" : ""}
+                    />
                 ))}
             </BentoGrid>
         </div>
