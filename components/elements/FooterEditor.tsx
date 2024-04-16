61b96eb0fe200e7756b1f041a2426b7a2cbc3515
Vito Andareas Manik <vitoandareas15@gmail.com>
2024-04-16 22:44:28 +0800

    feat: adding link to /editor for footer

--- components/elements/FooterEditor.tsx
+++ components/elements/FooterEditor.tsx
@@ -1,12 +1,32 @@
+"use client";
+import Link from "next/link";
 import { ModeToggle } from "../ui/mode-toggle";
 import DialogSignOut from "./DialogSignOut";
+import { buttonVariants } from "../ui/button";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
 
 type FooterEditorProps = {};
 
 const FooterEditor: React.FC<FooterEditorProps> = () => {
+    const pathname = usePathname();
+    console.log(pathname);
+
     return (
         <footer className="flex text-muted-foreground  items-center sticky backdrop-blur-sm w-full bottom-0 justify-center border-t bg-background/80">
             <ModeToggle />
+            <Link
+                className={cn(
+                    buttonVariants({
+                        variant: "link",
+                        className: "text-muted-foreground",
+                    }),
+                    { "text-white": pathname === "/editor" }
+                )}
+                href={"/editor"}
+            >
+                Editor Page
+            </Link>
             <DialogSignOut />
         </footer>
     );
